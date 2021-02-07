a2d3d26be83a8ab7affc5e216c8288cdc5648143
KUGAKU <githubgemini.0521@gmail.com>
2021-02-07 17:07:23 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 山本学 on 2021/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
